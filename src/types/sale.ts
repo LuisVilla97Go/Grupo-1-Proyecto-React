ba03be3d5a2bad02a58981edcc3d0650605869ed
LuisVilla97Go <luisantoniovilla97@gmail.com>
2026-07-30 11:45:44 -0500

    Se dejaron solo las librerias necesarias del proyecto

---
+++ src/types/sale.ts
@@ -0,0 +1,23 @@
+export interface SaleItem {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+  category?: string;
+}
+
+export interface CartItem extends SaleItem {
+  stockLimit: number;
+  image?: string;
+}
+
+export interface Sale {
+  id: string;
+  date: string;
+  items: SaleItem[];
+  subtotal: number;
+  igv: number;
+  total: number;
+  paymentMethod: "Efectivo" | "Tarjeta" | "Yape/Plin" | "Contraentrega";
+  seller: string;
+}
