e09a9db303aafdfda160d8d558f19c7c9dde03a0
LuisVilla97Go <luisantoniovilla97@gmail.com>
2026-08-02 22:05:51 -0500

    Se creo la carpeta script y se arreglo package.json, storefront y  products

---
+++ script/sync-data.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceDir = path.resolve(__dirname, "../server/data");
+const destDir = path.resolve(__dirname, "../public/api");
+
+if (!fs.existsSync(destDir)) {
+  fs.mkdirSync(destDir, { recursive: true });
+}
+
+const files = fs.readdirSync(sourceDir);
+for (const file of files) {
+  if (file.endsWith(".json")) {
+    fs.copyFileSync(path.join(sourceDir, file), path.join(destDir, file));
+    console.log(`[Sync] Copied ${file} from server/data/ to public/api/`);
+  }
+}
